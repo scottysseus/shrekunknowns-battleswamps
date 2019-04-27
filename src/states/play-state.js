ae35f80d7457e127a620cca54b1849b12839f4e7
Scott Weidenkopf <sweidenkopf@gmail.com>
2019-04-27 11:05:05 -0400

    cleaned up code a bit

--- src/states/play-state.js
+++ src/states/play-state.js
@@ -1,46 +1,114 @@
 export default function playState(game) {
     
-    let cursors;
-    let platforms;
-    let shrek;
+    const GRAVITY = 600*2;
+    const BOUNCE = 0.2;
 
+    // game stats
+    let level = 1;
+
+    // groups
+    let cursors;
+    let groundPlatform;
+
+    // characters
+    let shrek;
+    let donkey;
+
+    // flags
     let isShrekFacingLeft = true;
     let bouncing = false;
+
+    // player stats
+    let gold;
+    let inventory;
 
     function preload() {
         game.load.image('ground', 'src/assets/ground.png');
         game.load.image('groundTop', 'src/assets/groundTop.png');
         game.load.spritesheet('shrek', 'src/assets/shrek.png', 144/3, 72);
+        game.load.spritesheet('donkey', 'src/assets/donkey.png', 192/4, 48);
         game.load.image('tree1', 'src/assets/tree1.png');
         game.load.image('tree2', 'src/assets/tree2.png');
         game.load.image("berryBush", "src/assets/berryBush.png");
     }
     
     function create() {
-        addBackgroundScenery();
-        
         game.world.setBounds(0, 0, 6400, 512);
         game.stage.disableVisibilityChange = true;
         cursors = game.input.keyboard.createCursorKeys();
+        
+        addBackgroundScenery();
+        addEnemies();
+        addShrek();
+
+        //  We're going to be using physics, so enable the Arcade Physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.camera.follow(shrek, Phaser.Camera.FOLLOW_LOCKON);
+
+        addForegroundScenery();
+    }
+    
+    
+    function update() {
+        updateShrek();
+        moveDonkey();
+    }
+
+    function addBackgroundScenery() {
+        //  The groundPlatform group contains the ground and the 2 ledges we can jump on
+        groundPlatform = game.add.group();
+            
+        //  We will enable physics for any object that is created in this group
+        if (groundPlatform) {
+            groundPlatform.enableBody = true;
+        }
+        // Here we create the ground.
+        for(var i = 0; i < 65; ++i) {
+            var x = i * 256;
+            var y = game.world.height - 64;
+            var ground = groundPlatform.create(x, y, 'ground');
+            game.add.sprite(x, y - 32, 'groundTop');
+            ground.body.immovable = true;
+        }
+
+        game.add.sprite(150, game.world.height - 265, 'tree1');
+        game.add.sprite(300, game.world.height - 330, 'tree2');
+        game.add.sprite(100, game.world.height - 175, "berryBush");
+    }
+
+    function addShrek() {
         shrek = game.add.sprite(0,0,'shrek');
         shrek.anchor.setTo(0.5, 0.5);
         shrek.x = 150;
         shrek.y = 45;
         game.physics.arcade.enable(shrek);
-        shrek.body.bounce.y = 0.2;
-        shrek.body.gravity.y = 600*2;
+        shrek.body.bounce.y = BOUNCE;
+        shrek.body.gravity.y = GRAVITY;
         shrek.body.collideWorldBounds = true;
         shrek.animations.add('left', [0, 1, 2], 10, false);
         shrek.animations.add('right', [0,1,2], 10, false);
-
-        //  We're going to be using physics, so enable the Arcade Physics system
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.camera.follow(shrek, Phaser.Camera.FOLLOW_LOCKON);
     }
-    
-    
-    function update() {
-        var hitPlatform = game.physics.arcade.collide(shrek, platforms);
+
+    function addEnemies() {
+        donkey = game.add.sprite(0,0, 'donkey');
+        donkey.anchor.setTo(0.5, 0.5);
+
+        donkey.x = 150;
+        donkey.y = 45;
+        game.physics.arcade.enable(donkey);
+        donkey.body.bounce.y = BOUNCE;
+        donkey.body.gravity.y = GRAVITY;
+        donkey.body.collideWorldBounds = true;
+        donkey.animations.add('left', [0, 1, 2, 0], 12, false);
+        donkey.animations.add('right', [0, 1, 2, 0], 12, false);
+    }
+
+    function addForegroundScenery() {
+
+    }
+
+    function updateShrek() {
+        var isOnGround = game.physics.arcade.collide(shrek, groundPlatform);
         //  Reset the players velocity (movement)
         shrek.body.velocity.x = 0;
         shrek.body.bounce.y = bouncing ? 0.7 : 0.2;
@@ -69,52 +137,25 @@ export default function playState(game) {
         }
 
         //  Allow the shrek to jump if they are touching the ground.
-        if (cursors.up.isDown && shrek.body.touching.down && hitPlatform)
+        if (cursors.up.isDown && shrek.body.touching.down && isOnGround)
         {
             shrek.body.velocity.y = -350*1.5;
         }
 
         // bounce logic
-        if (hitPlatform) {
+        if (isOnGround) {
             bouncing = false;
         }
-        if (cursors.down.isDown && !hitPlatform && bouncing === false) {
+        if (cursors.down.isDown && !isOnGround && bouncing === false) {
             shrek.body.velocity.y = 900*1.25;
             bouncing = true;
         }
     }
 
-    function render() {
-
+    function moveDonkey() {
+        var isOnGround = game.physics.arcade.collide(donkey, groundPlatform);
+        
     }
 
-    function addBackgroundScenery() {
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = game.add.group();
-            
-        //  We will enable physics for any object that is created in this group
-        if (platforms) {
-            platforms.enableBody = true;
-        }
-
-        // Here we create the ground.
-
-        for(var i = 0; i < 65; ++i) {
-            var x = i * 256;
-            var y = game.world.height - 64;
-            var ground = platforms.create(x, y, 'ground');
-            game.add.sprite(x, y - 32, 'groundTop');
-            ground.body.immovable = true;
-        }
-
-        game.add.sprite(150, game.world.height - 265, 'tree1');
-        game.add.sprite(300, game.world.height - 330, 'tree2');
-        game.add.sprite(100, game.world.height - 175, "berryBush");
-    }
-
-    function addForegroundScenery() {
-
-    }
-
-    return {preload, create, update, render};
+    return {preload, create, update};
 }
